af5d17e22d3ab43af961f3ed8350041cf4025d7c
Leonardo Silva <yleo545@hotmail.com>
2023-04-09 19:09:21 -0300

    X14

---
+++ src/controle/For2.java
@@ -0,0 +1,13 @@
+package controle;
+
+public class For2 {
+	
+	public static void main(String[] args) {
+		
+		for (int x = 10; x >= 0; x-= 2) {
+			System.out.println("X = " + x);
+		}
+		
+	}
+
+}
