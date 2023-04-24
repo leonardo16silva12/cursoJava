a3590ba0fe58b909acf183c67af9ea1393547aa1
Leonardo Silva <yleo545@hotmail.com>
2023-04-24 16:09:41 -0300

    X27

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -5,21 +5,35 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
-	
+
 	Data() {
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+		// dia = 1;
+		// mes = 1;
+		// ano = 1970;
+
+		// "this()" - Chamar um construtor a partir de outro construtor, o que vai
+		// variar é o número de parâmetros passados e a ordem dos parâmetros.
+		// Sempre que utilizado como método, significa que está chamando um construtor.
+		// Só pode ser utilizado como método dentro de outro construtor.
+		// Não pode chamar dois this() em dois construtores, pois vai criar um loop, um
+		// chamando o outro indefinidamente,
+		// Sempre chamar um construtor que em algum deles crie um objeto corretamente
+		this(1, 1, 1970);
 	}
-	
-	Data(int diaInicial, int mesInicial, int anoInicial) {
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+
+	Data(int dia, int mesInicial, int anoInicial) {
+		// "this" - Acessar a váriavel de instância, mesmo se não houver conflito de
+		// nomes
+		// Não pode chamar o this em métodos static, porque o método é associado a
+		// classe e o this é associado a instância
+		// utilizado para chamar atributos, métodos 
+		this.dia = dia;
+		this.mes = mesInicial;
+		this.ano = anoInicial;
 	}
-	
+
 	String formatarData() {
 		return String.format("%d/%d/%d", dia, mes, ano);
 	}
-	
+
 }
--- src/classe/PrimeiroTrauma.java
+++ src/classe/PrimeiroTrauma.java
@@ -4,16 +4,16 @@ public class PrimeiroTrauma {
 
 	int a = 3;
 	static int b = 4;
-	
+
 	public static void main(String[] args) {
-		
+
 		// Acessar um atributo de instância criando outra instância
 		PrimeiroTrauma p = new PrimeiroTrauma();
 		System.out.println(p.a);
-		
+
 		// Ou é possível acessar, transformando o atributo em static
 		System.out.println(b);
-		
+
 	}
-	
+
 }
